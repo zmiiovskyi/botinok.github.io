ce3179a789a1957bb4ecdbe2b5e421516ecabe56
zmiiovskyi <zmiiovskyi.m@gmail.com>
2024-06-06 18:43:08 +0300

    click count

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 let clickCount = 0;
 
+// Завантажити кількість кліків з Local Storage при завантаженні сторінки
+window.onload = function() {
+    if (localStorage.getItem('clickCount')) {
+        clickCount = parseInt(localStorage.getItem('clickCount'), 10);
+        document.getElementById('clickCount').innerText = clickCount;
+    }
+};
+
+// Функція для збільшення кількості кліків і збереження їх у Local Storage
 function incrementCounter() {
     clickCount++;
     document.getElementById('clickCount').innerText = clickCount;
+    localStorage.setItem('clickCount', clickCount);
 }
